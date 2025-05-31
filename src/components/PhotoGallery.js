85b325adc9678a127f920cdadc908907e7c75333
nick bui <tronggiao1908@gmail.com>
2025-05-31 18:23:02 +0700

    fix image deployment issue

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -6,7 +6,7 @@ const PhotoGallery = () => {
     <div className="feature-box">
       <h2>📸 Photo Gallery</h2>
       <div className="photo-gallery">      
-        <img src="/images/facetime1.jpeg" alt="FaceTime" />
+        <img src="./images/facetime1.jpeg" alt="FaceTime" />
       </div>
       <p style={{ textAlign: 'center', marginTop: '10px' }}> My fav photo this month</p>
     </div>
