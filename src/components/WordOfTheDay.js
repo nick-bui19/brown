ebd09b2041ecfa2828e6ac03fbf922f600e68e0f
nick bui <tronggiao1908@gmail.com>
2025-06-01 10:38:04 +0700

    tweak word of the day component

--- src/components/WordOfTheDay.js
+++ src/components/WordOfTheDay.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 function WordOfTheDay() {
   const word = 'Trời ơi!'; 
-  const explanation = '天啊‼️。';
-  const explanation2 = '"Trời" means "天“, "ơi" in "anh ơi!" - an endearing way to call someone! -> Literally exclaiming to heaven hahahah!!'
+  const translation = '天啊‼️。';
+  const notes = '"Trời" means "天“, "ơi" in "anh ơi!" - an endearing way to call someone! -> Literally exclaiming to heaven hahahah!!'
 
   return (
     <div className="feature-box">
       <h2>📚 Word of the Day</h2>
-      <p><strong>{word}</strong> (verb)</p>
-      <p>{explanation}</p>
-      <p>{explanation2}</p>
+      <p><strong>{word}</strong></p>
+      <p>{translation}</p>
+      <p>{notes}</p>
     </div>
   );
 }
